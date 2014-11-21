afc4c0ccc38d39805723ad0e4a14c93aa956763b
jonathan eisenhamer <eisenhamer@mactest1.local>
2014-11-21 00:41:52 -0500

    Added conway-anim.py that used matplot animation library

---
+++ conway-anim.py
@@ -0,0 +1,51 @@
+import numpy as np
+
+from lib import fft_convolve2d
+
+import matplotlib.pyplot as plt
+import matplotlib.animation as anm
+
+def conway(state, k=None):
+    """
+    Conway's game of life state transition
+    """
+
+    # set up kernel if not given
+    if k == None:
+        m, n = state.shape
+        k = np.zeros((m, n))
+        k[m/2-1 : m/2+2, n/2-1 : n/2+2] = np.array([[1,1,1],[1,0,1],[1,1,1]])
+
+    # computes sums around each pixel
+    b = fft_convolve2d(state,k).round()
+    c = np.zeros(b.shape)
+
+    c[np.where((b == 2) & (state == 1))] = 1
+    c[np.where((b == 3) & (state == 1))] = 1
+
+    c[np.where((b == 3) & (state == 0))] = 1
+
+    # return new state
+    return c
+
+def data_gen():
+    global A
+    while True:
+        A = conway(A)
+        yield A
+
+def run(A):
+    img_plot.set_data(A)
+    return img_plot,
+
+if __name__ == "__main__":
+    # set up board
+    m, n = 100, 100
+    A = np.random.random(m*n).reshape((m, n)).round()
+
+    # plot each frame
+    fig = plt.figure()
+    img_plot = plt.imshow(A, interpolation="nearest", cmap = plt.cm.gray)
+
+    ani = anm.FuncAnimation(fig, run, data_gen, blit=True, interval=100)
+    plt.show()
